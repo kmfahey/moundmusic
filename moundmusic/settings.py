3b5fb60bf0df11f6d3371c6ad98d3b02da5687bd
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-01-06 16:43:01 -0800

    restored django admin pages and apps

--- moundmusic/settings.py
+++ moundmusic/settings.py
@@ -40,7 +40,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'albums.apps.AlbumsConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'albums',
 ]
 
 MIDDLEWARE = [
