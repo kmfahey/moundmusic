f35c0ac67ea59739ee9f563043070c2c18e65c09
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-01-13 12:48:08 -0800

    copied artists.models model class import solution to buyers.models, genres.models, sellers.models, songs.models, users.models

--- buyers/models.py
+++ buyers/models.py
@@ -2,4 +2,13 @@
 
 from django.db import models
 
-# Create your models here.
+from albums.models import Album, AlbumCover, AlbumGenreBridge, AlbumSongBridge, Artist, ArtistAlbumBridge, \
+        ArtistGenreBridge, ArtistSongBridge, BuyerAccount, Genre, SellerAccount, Song, SongGenreBridge, SongLyrics, \
+        ToBuyListing, ToSellListing, User, UserPassword
+
+
+for model_class in (Album, AlbumCover, AlbumGenreBridge, AlbumSongBridge, Artist, ArtistAlbumBridge, ArtistGenreBridge,
+                    ArtistSongBridge, BuyerAccount, Genre, SellerAccount, Song, SongGenreBridge, SongLyrics,
+                    ToBuyListing, ToSellListing, User, UserPassword):
+    model_class._meta.app_label = "buyers"
+
