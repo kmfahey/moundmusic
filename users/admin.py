a3479ca76220f8ed2611ce11b642b5d7645ba0b5
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-01-14 13:38:02 -0800

    updated all .admin modules with full model class lists

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,14 @@
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Album, AlbumCover, AlbumGenreBridge, AlbumSongBridge, Artist, ArtistAlbumBridge, \
+        ArtistGenreBridge, ArtistSongBridge, BuyerAccount, Genre, SellerAccount, Song, SongGenreBridge, SongLyrics, \
+        ToBuyListing, ToSellListing, User, UserPassword
+
+
+for model_class in (Album, AlbumCover, AlbumGenreBridge, AlbumSongBridge, Artist, ArtistAlbumBridge,
+                    ArtistGenreBridge, ArtistSongBridge, BuyerAccount, Genre, SellerAccount, Song, SongGenreBridge,
+                    SongLyrics, ToBuyListing, ToSellListing, User, UserPassword):
+    admin.site.register(model_class)
+
+
