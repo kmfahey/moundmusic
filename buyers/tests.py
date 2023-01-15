c34ad3163774cb5c69b88370934207d7214e1e54
Kerne MacDonald Fahey <kernefahey@protonmail.com>
2023-01-14 19:16:41 -0800

    removed /buyers and /sellers apps, will implement as subordinate endpoints to /users

--- buyers/tests.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-
-from django.test import TestCase
-
-# Create your tests here.
--- sellers/apps.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-from django.apps import AppConfig
-
-
-class SellersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sellers'
